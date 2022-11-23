7383e58e6104017275b68af14806c253015892a4
Joan Cardona <joancardonasa@gmail.com>
2022-11-23 17:48:04 +0000

    Work on matrix manipulation for uploaded images

--- app/image_processing/processor.py
+++ app/image_processing/processor.py
@@ -1,6 +1,18 @@
+import numpy as np
 from PIL import Image
+
+import functools
 
 def check_image_size(filename: str) -> None:
     im = Image.open(filename)
     print("Successfully loaded image!")
     print(f"Image size: {im.size[0]}x{im.size[1]}")
+
+    # Load an ndarray of (columns, rows, rgb[a])
+    # When a exists, what to do?
+    pixels = np.asarray(im)
+    print(pixels.shape)
+
+    # Average out each pixel by its rgb values. Should alpha be considered?
+    pixel_matrix = np.mean(pixels, axis=2)
+    print(pixel_matrix.shape)
