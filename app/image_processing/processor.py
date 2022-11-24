2867b3e2eb0e136b9a56a7e5cbf72365b825eea0
Joan Cardona <joancardonasa@gmail.com>
2022-11-24 10:24:40 +0000

    Do file validation + cleanup processing

--- app/image_processing/processor.py
+++ app/image_processing/processor.py
@@ -3,46 +3,62 @@ from PIL import Image
 
 import functools
 
+import logging
+logger = logging.getLogger(__name__)
+
+DEFAULT_TARGET_WIDTH = 160
+
 ASCII_BY_BRIGHTNESS = '`"i+tXZ*W$' #'`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 
-def normalize(x, new_range=(0, 1)): #x is an array. Default range is between zero and one
-    xmin, xmax = np.min(x), np.max(x) #get max and min from input array
-    norm = (x - xmin)/(xmax - xmin) # scale between zero and one
 
-    if new_range == (0, 1):
-        return(norm) # wanted range is the same as norm
-    elif new_range != (0, 1):
-        return norm * (new_range[1] - new_range[0]) + new_range[0]
+def process_image(image: Image, target_width: float = DEFAULT_TARGET_WIDTH) -> None:
+    (raw_width, raw_height) = image.width, image.height
+
+    if target_width < raw_width:
+        target_height = int((raw_height / raw_width) * target_width)
+        image = image.resize((target_width, target_height))
+
+    pixel_matrix = np.asarray(image)
+    brightness_matrix = np.mean(pixel_matrix, axis=2)
+    norm_brightness_matrix = normalize_array_values_between_range(
+        brightness_matrix, new_range=(0, len(ASCII_BY_BRIGHTNESS)-1))
+
+    print(norm_brightness_matrix.shape)
+    # (width, height) = (raw_im.width // 10, raw_im.height // 10)
+    # im = raw_im.resize((width, height))
+    # print("Successfully loaded image!")
+    # print(f"Image size: {width}x{height}")
+
+    # # Load an ndarray of (columns, rows, rgb[a])
+    # # When a exists, what to do?
+    # pixels = np.asarray(im)
+    # print(pixels.shape)
+
+    # # Average out each pixel by its rgb values. Should alpha be considered?
+    # brightness_matrix = np.mean(pixels, axis=2)
+    # normalized_brightness_matrix = normalize(brightness_matrix, new_range=(
+    #     0, len(ASCII_BY_BRIGHTNESS)-1))
+
+    # height, width = normalized_brightness_matrix.shape
+
+    # img_string = ''
+    # for i in range(height):
+    #     for j in range(width):
+    #         brightness_pixel_val = normalized_brightness_matrix[i, j]
+    #         # We don't need to keep pixel brightness val
+    #         #img_dict[(i, j)] = brightness_pixel_val
+    #         img_string += ASCII_BY_BRIGHTNESS[int(brightness_pixel_val)] * 3
+    # print(img_string)
 
 
-def map_to_ascii(brightness: float) -> str:
+def normalize_array_values_between_range(x, new_range=(0, 1)):
+    if new_range == (0, 1): return(norm)
+
+    xmin, xmax = np.min(x), np.max(x)
+    norm = (x - xmin)/(xmax - xmin)
+
+    return norm * (new_range[1] - new_range[0]) + new_range[0]
+
+
+def map_to_ascii(pixel_value: float) -> str:
     pass
-
-
-def check_image_size(filename: str) -> None:
-    raw_im = Image.open(filename)
-    (width, height) = (raw_im.width // 4, raw_im.height // 4)
-    im = raw_im.resize((width, height))
-    print("Successfully loaded image!")
-    print(f"Image size: {width}x{height}")
-
-    # Load an ndarray of (columns, rows, rgb[a])
-    # When a exists, what to do?
-    pixels = np.asarray(im)
-    print(pixels.shape)
-
-    # Average out each pixel by its rgb values. Should alpha be considered?
-    brightness_matrix = np.mean(pixels, axis=2)
-    normalized_brightness_matrix = normalize(brightness_matrix, new_range=(
-        0, len(ASCII_BY_BRIGHTNESS)-1))
-
-    height, width = normalized_brightness_matrix.shape
-
-    img_string = ''
-    for i in range(height):
-        for j in range(width):
-            brightness_pixel_val = normalized_brightness_matrix[i, j]
-            # We don't need to keep pixel brightness val
-            #img_dict[(i, j)] = brightness_pixel_val
-            img_string += ASCII_BY_BRIGHTNESS[int(brightness_pixel_val)] * 3
-    print(img_string)
--- app/main.py
+++ app/main.py
@@ -1,32 +1,40 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+import logging
 import os
-from random import randint
 import uuid
 
-from image_processing.processor import check_image_size
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from PIL import Image, UnidentifiedImageError
 
-IMAGEDIR = "tmp_images/"
+from image_processing.processor import process_image
 
+logger = logging.getLogger(__name__)
+
+IMAGEDIR = 'tmp_images/'
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
+
 def read_root():
-    return {"Hello": "World"}
+    return {'Hello': 'World'}
 
 
-@app.post("/images/")
+@app.post('/images/')
 async def create_upload_file(file: UploadFile = File(...)):
-    # No extension validation yet, accepts anything
-    file.filename = f"{uuid.uuid4()}"
+    file.filename = f'{uuid.uuid4()}'
     contents = await file.read()
 
-    destination_file = f"../{IMAGEDIR}{file.filename}"
-    with open(destination_file, "wb") as f:
+    destination_file = f'../{IMAGEDIR}{file.filename}'
+    with open(destination_file, 'wb') as f:
         f.write(contents)
 
-    check_image_size(destination_file)
+    try:
+        raw_im = Image.open(destination_file)
+    except UnidentifiedImageError:
+        error_msg = 'File is not an image file'
+        raise HTTPException(status_code=400, detail=error_msg)
 
-    return {"filename": file.filename}
+    process_image(raw_im)
 
+    return {'filename': file.filename}
